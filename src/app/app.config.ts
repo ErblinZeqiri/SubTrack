013bc13bc2f556a56346df986353635f804c431a
Erblin Zeqiri <erblin.zeqiri17@gmail.com>
2024-07-10 13:24:37 +0200

    Ajout d'Ionic et sa config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
-
 import { routes } from "./app.routes";
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from "@angular/common/http";
+import {
+  provideIonicAngular
+} from "@ionic/angular/standalone";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideIonicAngular({}),
   ],
 };
