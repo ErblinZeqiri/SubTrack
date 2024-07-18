8b7acb0e36210a68c6cfa34d54893995feb08add
zeqirie <erblin.zeqiri17@gmail.com>
2024-07-18 23:56:30 +0200

    Implémentation des validations du formulaire d'ajout

--- src/app/add-sub/add-sub.component.ts
+++ src/app/add-sub/add-sub.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { Component, NgModule, ViewChild } from '@angular/core';
+import {
+  FormGroup,
+  FormsModule,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { IonicModule, IonDatetime } from '@ionic/angular';
 import { IonInput } from '@ionic/angular/standalone';
 import {
   Company,
@@ -9,25 +15,29 @@ import {
 import { CommonModule } from '@angular/common';
 import { collection, doc, getFirestore, setDoc } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-add-sub',
   standalone: true,
-  imports: [CommonModule, IonicModule, FormsModule],
+  imports: [CommonModule, IonicModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-sub.component.html',
   styleUrls: ['./add-sub.component.scss'],
 })
 export class AddSubComponent {
   @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
-  inputModel = '';
+  @ViewChild('datetime', { static: false }) datetime!: IonDatetime;
+  companySelected = '';
   logo = '';
   domain = '';
   filteredOptions: Company[] = [];
   selectedOption: Company | null = null;
   price: number | undefined;
   selectedCategory: string | undefined;
-  nextPaymentDate: string | undefined;
+  selectedRenewal: string | undefined;
+  nextPaymentDate: Date | undefined;
   subscriptionCategories: string[] = [
+    'Divertissement',
     'Indispensable',
     'Streaming',
     'Presse',
@@ -40,16 +50,58 @@ export class AddSubComponent {
     'Finance',
     'Voyage',
   ];
+  subscriptionRenewal: string[] = [
+    'Hebdomadaire',
+    'Mensuel',
+    'Trimestriel',
+    'Semestriel',
+    'Annuel',
+  ];
+  today = new Date();
+  selectedDate: Date | undefined = this.today;
+
+  public signinForm!: FormGroup;
 
   constructor(
     private companySuggestionsService: CompanySuggestionsService,
-    private readonly _router: Router
-  ) {}
+    private readonly _router: Router,
+    private loadingCtrl: LoadingController
+  ) {
+    this.signinForm = new FormGroup({
+      companySelected: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+      price: new FormControl('', Validators.compose([Validators.required])),
+      selectedCategory: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+      selectedRenewal: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+      nextPaymentDate: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+    });
+  }
+
+  selectDate() {
+    this.datetime.confirm();
+    this.selectedDate = this.nextPaymentDate;
+  }
+
+  resetDateTime() {
+    this.datetime.reset();
+    this.selectedDate = this.today;
+  }
 
   onInput(ev: any) {
     const value = ev.target.value;
     const filteredValue = value.replace(/[^a-zA-Z0-9 ]+/g, '');
-    this.inputModel = filteredValue;
+    this.companySelected = filteredValue;
 
     if (filteredValue.length > 1) {
       this.companySuggestionsService
@@ -63,39 +115,45 @@ export class AddSubComponent {
   }
 
   selectOption(option: Company) {
-    this.inputModel = option.name;
+    this.companySelected = option.name;
     this.logo = option.logo;
     this.domain = option.domain;
     this.filteredOptions = [];
   }
 
-  async onSubmit() {
-    try {
-      const user: any = localStorage.getItem('user');
-      const localStorageData: any = JSON.parse(user);
+  resetForm() {
+    this.signinForm.reset();
+    this.logo = '';
+    this.domain = '';
+  }
 
-      if (!localStorageData || !localStorageData.uid) {
-        throw new Error('User ID not found in localStorage');
-      }
+  async onSubmit() {
+    const user: any = localStorage.getItem('user');
+    const localStorageData: any = JSON.parse(user);
+
+    if (!localStorageData || !localStorageData.uid) {
+      throw new Error('User ID not found in localStorage');
+    }
+
+    if (this.signinForm.valid) {
+      const loading = await this.loadingCtrl.create({
+        message: 'Connexion...',
+      });
+      console.log(this.signinForm.valid);
+
       const formData = {
-        companyName: this.inputModel,
+        ...this.signinForm.value,
         logo: this.logo,
         domain: this.domain,
-        price: this.price,
-        category: this.selectedCategory,
-        nextPaymentDate: this.nextPaymentDate,
         paymentHistory: [],
         userID: localStorageData.uid,
       };
+
       const db = getFirestore();
       await setDoc(doc(collection(db, 'subscriptions')), formData);
+
+      this.resetForm();
       this._router.navigate(['/home']);
-    } catch (error) {
-      console.error('Error writing document: ', error);
     }
   }
-
-  // trackByFn(index: number, item: any) {
-  //   return item.name;
-  // }
 }
