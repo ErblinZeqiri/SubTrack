e3a85fb637cd710cf3d623c4de9ee1fb2e6b8181
Erblin Zeqiri <erblin.zeqiri17@gmail.com>
2024-07-18 12:14:06 +0200

    Changement de appId et appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'New Ionic App',
+  appId: 'com.zeqiri.subtrack',
+  appName: 'SubTrack',
   webDir: 'www'
 };
 
