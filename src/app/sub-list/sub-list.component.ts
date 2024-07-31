879ae966d6c5537deb434c2a88aa72cec32e6c32
Erblin Zeqiri <erblin.zeqiri17@gmail.com>
2024-07-31 11:33:31 +0200

    Isolation du donut dans un composant

--- src/app/sub-list/sub-list.component.ts
+++ src/app/sub-list/sub-list.component.ts
@@ -8,19 +8,6 @@ import { map, Observable } from 'rxjs';
 import { DataService } from '../services/data/data.service';
 import { AuthService } from '../services/auth/auth.service';
 import {
-  ApexNonAxisChartSeries,
-  ApexResponsive,
-  ApexChart,
-  ApexDataLabels,
-  ApexLegend,
-  ApexStroke,
-  ApexPlotOptions,
-  ApexStates,
-  ApexTheme,
-  ApexTitleSubtitle,
-} from 'ng-apexcharts';
-import { ChartComponent, NgApexchartsModule } from 'ng-apexcharts';
-import {
   IonHeader,
   IonToolbar,
   IonTitle,
@@ -31,21 +18,8 @@ import {
   IonRefresher,
   IonRefresherContent,
 } from '@ionic/angular/standalone';
+import { DonutChartComponent } from '../donut-chart/donut-chart.component'
 
-export type ChartOptions = {
-  series: ApexNonAxisChartSeries;
-  chart: ApexChart;
-  responsive: ApexResponsive[];
-  labels: any;
-  fill: any;
-  stroke: ApexStroke;
-  states: ApexStates;
-  legend: ApexLegend;
-  title: ApexTitleSubtitle;
-  theme: ApexTheme;
-  plotOptions: ApexPlotOptions;
-  dataLabels: ApexDataLabels;
-};
 
 @Component({
   selector: 'app-sub-list',
@@ -63,7 +37,7 @@ export type ChartOptions = {
     CommonModule,
     NgOptimizedImage,
     RouterLink,
-    NgApexchartsModule,
+    DonutChartComponent
   ],
   templateUrl: './sub-list.component.html',
   styleUrls: ['./sub-list.component.css'],
@@ -76,10 +50,6 @@ export class SubListComponent implements OnInit {
   userData$!: Observable<User[]>;
   userSubData$!: Observable<Subscription[]>;
   credentials: string | null = localStorage.getItem('user');
-  data: any;
-  options: any;
-  @ViewChild('chart') chart!: ChartComponent;
-  public chartOptions!: Partial<ChartOptions> | any;
 
   constructor(
     private readonly expenses: ExepensesService,
@@ -105,166 +75,6 @@ export class SubListComponent implements OnInit {
             this.expenses.getCurrentExpensesYear(userSubData)
           )
         );
-
-        let labels: string[] = [];
-        let series: number[] = [];
-
-        this.userSubData$.subscribe((subscriptions) => {
-          subscriptions.forEach((sub) => {
-            labels.push(sub.companyName);
-            series.push(sub.amount);
-          });
-        });
-
-        this.chartOptions = {
-          series: series,
-          chart: {
-            width: '30%',
-            type: 'donut',
-            dropShadow: {
-              enabled: true,
-              color: '#111',
-              top: -1,
-              left: 3,
-              blur: 3,
-              opacity: 0.2,
-            },
-            animations: {
-              enabled: true,
-              easing: 'linear',
-              speed: 500,
-              animateGradually: {
-                enabled: true,
-                delay: 150,
-              },
-              dynamicAnimation: {
-                enabled: true,
-                speed: 350,
-              },
-            },
-          },
-          stroke: {
-            width: 0,
-          },
-          plotOptions: {
-            pie: {
-              donut: {
-                size: '70%',
-                labels: {
-                  show: true,
-                  total: {
-                    showAlways: true,
-                    show: true,
-                    label: 'Total',
-                    fontSize: '14px',
-                    color: '#555',
-                  },
-                },
-              },
-            },
-          },
-          labels: labels,
-          dataLabels: {
-            enabled: true,
-            style: {
-              fontSize: '12px',
-            },
-            dropShadow: {
-              blur: 3,
-              opacity: 0.8,
-            },
-          },
-          fill: {
-            type: 'pattern',
-            opacity: 1,
-            pattern: {
-              enabled: true,
-              style: [],
-            },
-          },
-          states: {
-            hover: {
-              filter: {
-                type: 'none',
-              },
-            },
-          },
-          theme: {
-            palette: 'palette2',
-          },
-          title: {
-            text: 'Résumé de vos abonnements',
-            align: 'center',
-            floating: false,
-            margin: 30,
-            style: {
-              fontSize: '16px',
-              color: '#333',
-            },
-          },
-          legend: {
-            position: 'bottom',
-            horizontalAlign: 'center',
-            floating: false,
-            fontSize: '12px',
-            offsetY: 20,
-            itemMargin: {
-              horizontal: 10,
-              vertical: 5,
-            },
-          },
-          responsive: [
-            {
-              breakpoint: 500,
-              options: {
-                chart: {
-                  width: '99%',
-                },
-                title: {
-                  text: 'Résumé de vos abonnements',
-                  align: 'center',
-                  margin: 20,
-                  style: {
-                    fontSize: '14px',
-                    color: '#333',
-                  },
-                },
-                legend: {
-                  position: 'bottom',
-                  horizontalAlign: 'center',
-                  fontSize: '10px',
-                  offsetY: 10,
-                  itemMargin: {
-                    horizontal: 5,
-                    vertical: 5,
-                  },
-                },
-                plotOptions: {
-                  pie: {
-                    donut: {
-                      size: '50%',
-                      labels: {
-                        show: true,
-                        total: {
-                          showAlways: true,
-                          show: true,
-                          label: 'Total',
-                          fontSize: '12px',
-                        },
-                      },
-                    },
-                  },
-                },
-                dataLabels: {
-                  enabled: true,
-                  style: {
-                    fontSize: '10px',
-                  },
-                },
-              },
-            },
-          ],
-        };
       }
     }
   }
