b4bb58b2f3bf15bacc26e4d6ca212abf4831fc6d
zeqirie <erblin.zeqiri17@gmail.com>
2024-08-13 19:11:52 +0200

    Ajout des condif angular dans appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,49 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { routes } from "./app.routes";
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { DatePipe } from '@angular/common';
+import {
+  RouteReuseStrategy,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
-} from "@angular/common/http";
-import {
-  provideIonicAngular
-} from "@ionic/angular/standalone";
-import { DatePipe } from "@angular/common";
+} from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    DatePipe
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyBPpboh7pXaboBXRILn2-_dmXxAM7ZOt2s',
+        authDomain: 'subtrack-330ce.firebaseapp.com',
+        projectId: 'subtrack-330ce',
+        storageBucket: 'subtrack-330ce.appspot.com',
+        messagingSenderId: '369263570865',
+        appId: '1:369263570865:web:53efbd01fd38a7ab5b1790',
+        measurementId: 'G-GCQPPGHLW0',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    DatePipe,
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  RouteReuseStrategy,
-  provideRouter,
-  withPreloading,
-  PreloadAllModules,
-} from '@angular/router';
-import {
-  IonicRouteStrategy,
-  provideIonicAngular,
-} from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { isDevMode } from '@angular/core';
-import { provideServiceWorker } from '@angular/service-worker';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withInterceptorsFromDi()),
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideFirebaseApp(() =>
-      initializeApp({
-        apiKey: 'AIzaSyBPpboh7pXaboBXRILn2-_dmXxAM7ZOt2s',
-        authDomain: 'subtrack-330ce.firebaseapp.com',
-        projectId: 'subtrack-330ce',
-        storageBucket: 'subtrack-330ce.appspot.com',
-        messagingSenderId: '369263570865',
-        appId: '1:369263570865:web:53efbd01fd38a7ab5b1790',
-        measurementId: 'G-GCQPPGHLW0',
-      })
-    ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
