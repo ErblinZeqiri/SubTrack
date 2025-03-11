8f2e9d152c0b273dca3043e54a2a0d08ea2ada6f
zeqirie <erblin.zeqiri17@gmail.com>
2025-03-11 01:15:12 +0100

    Ajout des données de la conf firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBPpboh7pXaboBXRILn2-_dmXxAM7ZOt2s',
+    authDomain: 'subtrack-330ce.firebaseapp.com',
+    projectId: 'subtrack-330ce',
+    storageBucket: 'subtrack-330ce.firebasestorage.app',
+    messagingSenderId: '369263570865',
+    appId: '1:369263570865:web:53efbd01fd38a7ab5b1790',
+    measurementId: 'G-GCQPPGHLW0',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBPpboh7pXaboBXRILn2-_dmXxAM7ZOt2s',
+    authDomain: 'subtrack-330ce.firebaseapp.com',
+    projectId: 'subtrack-330ce',
+    storageBucket: 'subtrack-330ce.firebasestorage.app',
+    messagingSenderId: '369263570865',
+    appId: '1:369263570865:web:53efbd01fd38a7ab5b1790',
+    measurementId: 'G-GCQPPGHLW0',
+  },
 };
 
 /*
